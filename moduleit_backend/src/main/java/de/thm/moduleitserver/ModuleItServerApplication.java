562a1043c5f3ed47daca7e0a9b6ccb3788fc65db
Abdulrahman Othman <gazixq@gmail.com>
2024-08-07 23:45:27 +0200

    project init

---
+++ moduleit_backend/src/main/java/de/thm/moduleitserver/ModuleItServerApplication.java
@@ -0,0 +1,13 @@
+package de.thm.moduleitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleItServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleItServerApplication.class, args);
+    }
+
+}
---
+++ moduleit_backend/src/test/java/de/thm/moduleitserver/ModuleItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thm.moduleitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleItServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
